68f094f7a3b42fbd8f84e93d32d53dff6ab276df
imakash3011 <imakashpatel3011@gmail.com>
2019-10-09 21:33:28 +0530

    Node.js Starts here

---
+++ Lecture_12/Node.js/Browser-Multifile/script.js
@@ -0,0 +1,11 @@
+// console.log(global)
+// console.log(process)
+// console.log(global.process==process)
+
+// let x=Number(10)
+// console.log(x);
+
+console.log(add(5,5))
+console.log(add(5,4))
+
+
